db60f45bdaf4cf9f922124c99b5d9269eb12f2ee
logicmyth <staley77@protonmail.com>
2017-07-20 15:49:19 -0400

    complete Q3+Q4

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -29,3 +29,40 @@ var reverseWords = function(string) {
 }
 
 //Q3
+
+let countDivs = function(str) {
+  return $("#outerDiv").length;
+};
+
+countDivs();
+
+//Q4
+// var persons = [
+//   { firstname : 'George', lastname: 'Kolev', age: 32},
+//   { firstname : 'Bay', lastname: 'Ivan', age: 81},
+//   { firstname : 'Baba', lastname: 'Ginka', age: 40}]
+// findYoungestPerson(persons);
+
+var findYoungestPerson(persons) {
+  // for (i=0; i > persons.legth; i++) {
+  //   var compOne = i[2];
+  //   var co
+  //   var compTwo = i[2] +1;
+  //   if (compOne > compTwo) {
+  var xVals = persons.map(function(obj) { return obj.[2]; });
+  var min = Math.min.apply(null, xVals);
+      return "The youngest person is min[0][1] min[0][2]"
+    }
+  }
+}
+
+//Q5
+// var persons = {
+//     people.push(new Person("Scott", "Guthrie", 38));
+//     people.push(new Person("Scott", "Johns", 36));
+//     people.push(new Person("Scott", "Hanselman", 39));
+//     people.push(new Person("Jesse", "Liberty", 57));
+//     people.push(new Person("Jon", "Skeet", 38));
+// };
+
+var persons = new Object {};
