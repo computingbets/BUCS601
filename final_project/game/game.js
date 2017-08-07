2ae885fd0dbfff0be5cc15fc846f9467fbba7e6e
logicmyth <staley77@protonmail.com>
2017-08-07 11:49:50 -0400

    add tree favicons to html files. add answer area divs to game.html. start making jquery drag and drop functionality to game.js

---
+++ final_project/game/game.js
@@ -0,0 +1,16 @@
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+// let sassCorrect = function() {
+//   if ($.sDrop.hasClass()
+// }
+
+function init() {
+  $('#makeMeDraggable').draggable();
+  $('#makeMeDroppable').droppable( {
+    drop: handleDropEvent
+  } );
+}
