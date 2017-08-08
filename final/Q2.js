b283491d37cf063874f458c543f8075583a912fc
logicmyth <staley77@protonmail.com>
2017-08-08 18:52:01 -0400

    add Q2,Q3

---
+++ final/Q2.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let getSearchResults = function (firstName, lastName) {
+
+};
+
+let saveResultsToLocalStorage = function (dataArray, keyValue) {
+
+};
+
+const maxResults = 200;
+
+let serveErrorMessage = function (timoutPeriod, durationAmount) {
+
+};
+
+let determineTaxRate = function (state, local, federal) {
+
+};
+
+module.exports = {
+ getSearchResults,
+ saveResultsToLocalStorage,
+ maxResults,
+ serveErrorMessage,
+ determineTaxRate,
+};
