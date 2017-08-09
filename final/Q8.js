75c08dd06d95624d5b77480de751085c44599b92
logicmyth <staley77@protonmail.com>
2017-08-08 20:02:48 -0400

    add Q8,Q9

---
+++ final/Q8.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let add20 = function (whatever) {
+  if (typeof whatever = 'string') {
+    return whatever.append('20');
+  } else if (typeof whatever = 'number') {
+    return whatever + 20
+  } else {
+    return 20
+  };
+};
+
+function add20FatArrow (whatever) => {
+  if (typeof whatever = 'string') {
+    return whatever.append('20');
+  } else if (typeof whatever = 'number') {
+    return whatever + 20
+  } else {
+    return 20
+  };
+};
